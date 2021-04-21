12b06b34c2148f6aebc3e8f83f17717f7a33a3f4
sbaraldo <sbaraldo@roc-dev.com>
2021-04-21 16:51:34 +0200

    Movement player and fixed the camera position on the player

---
+++ GameExam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //Playermovement
+        transform.Translate(Vector3.right * horizontalInput * Time.fixedDeltaTime * speed);
+        transform.Translate(Vector3.forward * Time.fixedDeltaTime * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Player input
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
