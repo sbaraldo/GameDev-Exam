acb56196095ebf95936798dedca89a32288529ff
sbaraldo <sbaraldo@roc-dev.com>
2021-04-26 17:00:32 +0200

    Made variables and methodes for the UserInterface

---
+++ GameExam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI scoreText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        
+    }
+}
