26c20686d70a68f06cd7eb9d9d25926366024cc0
sbaraldo <sbaraldo@roc-dev.com>
2021-04-29 18:34:16 +0200

    Obstacle get destroyed if it falls of the map

--- GameExam/Assets/Scripts/MoveBack.cs
+++ GameExam/Assets/Scripts/MoveBack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MoveBack : MonoBehaviour
 {
     private float speed = 10.0f;
+    private float backBound = -5.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,11 @@ public class MoveBack : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.back * Time.deltaTime * speed);
+
+        // If the position z is smaller than -5 it destroy the obstacle
+        if(transform.position.z < backBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
