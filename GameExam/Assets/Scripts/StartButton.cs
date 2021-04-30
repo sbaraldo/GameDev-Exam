72604a1f6a72cad49de0ab36afdc7d456980ef99
sbaraldo <sbaraldo@roc-dev.com>
2021-04-30 16:53:35 +0200

    Script to start the game if u click on the start button

---
+++ GameExam/Assets/Scripts/StartButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        button.onClick.AddListener(GameStart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GameStart()
+    {
+        gameManager.StartGame();
+    }
+}
