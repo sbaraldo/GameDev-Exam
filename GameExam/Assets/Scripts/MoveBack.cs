4a2ced59dee37c1020f496e9635fed762efc6a0d
sbaraldo <sbaraldo@roc-dev.com>
2021-05-11 18:23:35 +0200

    Gem get destroyed if out of bounds

--- GameExam/Assets/Scripts/MoveBack.cs
+++ GameExam/Assets/Scripts/MoveBack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveBack : MonoBehaviour
 {
-    private float speed = 10.0f;
+    public float speed = 10.0f;
     private float backBound = -5.0f;
 
     // Start is called before the first frame update
@@ -18,8 +18,13 @@ public class MoveBack : MonoBehaviour
     {
         transform.Translate(Vector3.back * Time.deltaTime * speed);
 
-        // If the position z is smaller than -5 it destroy the obstacle
+        // If the position z is smaller than -5 it destroy the obstacle / als positie z kleiner is dan -5 het destroy de obstakel
         if(transform.position.z < backBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+
+        if(transform.position.z < backBound && gameObject.CompareTag("Gem"))
         {
             Destroy(gameObject);
         }
