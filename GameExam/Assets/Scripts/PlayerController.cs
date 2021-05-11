94dcf093bb8a7016ea62fdf81ef2399c413f249d
sbaraldo <sbaraldo@roc-dev.com>
2021-05-11 18:22:39 +0200

    Can collect the gem

--- GameExam/Assets/Scripts/PlayerController.cs
+++ GameExam/Assets/Scripts/PlayerController.cs
@@ -11,20 +11,22 @@ public class PlayerController : MonoBehaviour
     public AudioClip crashSound;
     private AudioSource playerAudio;
     private GameManager gameManager;
+    private MoveBack cube;
 
     // Start is called before the first frame update
     void Start()
     {
         playerAudio = GetComponent<AudioSource>();
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        cube = GameObject.Find("Obstacle").GetComponent<MoveBack>();
     }
 
     void FixedUpdate()
     {
-        //Playermovement
+        //Playermovement / Speler beweging
         transform.Translate(Vector3.right * horizontalInput * Time.fixedDeltaTime * speed);
 
-        //Keep the player on the ground so he don't fall of the map
+        //Keep the player on the ground so he don't fall of the map / speler kan niet van de map af vallen
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -40,19 +42,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Player input
+        //Player input / speler invoer
         horizontalInput = Input.GetAxis("Horizontal");
     }
 
     private void OnCollisionEnter( Collision collision)
     {
-        // If player collide with the obstacle the player is dead, hear crashsound and see particle 
+        // If player collide with the obstacle the player is dead, hear crashsound and see particle
+        // als speler tegen object botst de speler is dood, je hoort crashsound en ziet een particle
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
             AudioSource.PlayClipAtPoint(crashSound, transform.position);
             Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
             gameManager.GameOver();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Gem"))
+        {
+            Destroy(other.gameObject);
+            gameManager.score += 1;
+            gameManager.UpdateScore(gameManager.score);
+            cube.speed += 2f;
         }
     }
 }
