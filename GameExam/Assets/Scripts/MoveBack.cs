296d92aaf5f17a2c3044eeacc218374d9846051c
sbaraldo <sbaraldo@roc-dev.com>
2021-05-20 21:11:24 +0200

    obstacle and gem speed is increased

--- GameExam/Assets/Scripts/MoveBack.cs
+++ GameExam/Assets/Scripts/MoveBack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveBack : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 15.0f;
     private float backBound = -5.0f;
 
     // Start is called before the first frame update
